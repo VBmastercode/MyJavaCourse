29c9290c4aeece83a4a140f1b43a3cca213b1c7b
Vicktor <lamperuge.lelush@gmail.com>
2020-04-10 15:02:41 +0300

    ver 0.4

--- Task7/src/Main.java
+++ Task7/src/Main.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        String s1,s2;
+        String s1, s2;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Input first string");
@@ -12,15 +14,64 @@ public class Main {
         System.out.println("Input second string");
         s2 = sc.nextLine();
 
+        // methode 1
+
         int s1lenght = s1.length();
         int s2lenght = s2.length();
 
         for (int i = 0; i <= (s2lenght - s1lenght); i++) {
-            String str3 = s2.substring(i,(i+s1lenght));
-            if (str3.equals(s1) ) {
+            String str3 = s2.substring(i, (i + s1lenght));
+            if (str3.equals(s1)) {
                 System.out.println(s2 + " <include> " + s1);
             }
         }
 
+        // methode 2
+
+        int index1 = -1;
+        index1 = s2.indexOf(s1);
+        if (index1 >= 0) {
+            System.out.println(s2 + " <include> " + s1);
+        } else {
+            System.out.println(s2 + " <doesnt include> " + s1);
+        }
+
+        // names
+
+        String sinit, sname, ssurname, smlname;
+
+        System.out.println("Input surname name and middle name, please");
+        sinit = sc.nextLine();
+
+        String[] parts = sinit.split(" ");
+
+        ssurname = parts[0];
+        sname = parts[1];
+        smlname = parts[2];
+
+        System.out.println(ssurname + " " + sname.charAt(0) + "." + smlname.charAt(0));
+        System.out.println(sname);
+        System.out.println(sname + " " + smlname + " " + ssurname);
+
+        // regex
+
+        String sregname;
+        Pattern p = Pattern.compile("^[a-z0-9_-]{3,15}$");
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Input name, please");
+            sregname = sc.nextLine();
+            Matcher m = p.matcher(sregname);
+
+            if (m.matches()) {
+                System.out.println("Name " + sregname + " is valid");
+            } else {
+                System.out.println("Oops, not valid name");
+            }
+
+        }
     }
+
+
 }
+
